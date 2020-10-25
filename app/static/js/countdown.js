3df66a1e7f89380e5581d6d4e865b5940e6022ef
AquaUseful <l.vladik@outlook.com>
2020-10-26 01:16:16 +0300

    Changeble time unit indicators

--- app/static/js/countdown.js
+++ app/static/js/countdown.js
@@ -1,9 +1,14 @@
+let DAY_STR = " дн"
+let HOUR_STR = " ч"
+let MINUTE_STR = " мин"
+let SECOND_STR = " с"
+
 function setCounterText(distMilliseconds) {
   let days = Math.floor(distMilliseconds / (1000 * 60 * 60 * 24));
   let hours = Math.floor(distMilliseconds % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
   let minutes = Math.floor(distMilliseconds % (1000 * 60 * 60) / (1000 * 60));
   let seconds = Math.floor(distMilliseconds % (1000 * 60) / 1000);
-  text = `${days}d ${hours}h ${minutes}m ${seconds}s`
+  text = `${days}${DAY_STR} ${hours}${HOUR_STR} ${minutes}${MINUTE_STR} ${seconds}${SECOND_STR}`
   $(".counter").text(text);
 };
 
