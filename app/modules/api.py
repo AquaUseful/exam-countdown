b9c7b9596e778352154090cbbee53357e078f2f8
AquaUseful <l.vladik@outlook.com>
2020-10-26 00:59:46 +0300

    Allow setting time parameters via env values

--- app/modules/api.py
+++ app/modules/api.py
@@ -1,32 +1,49 @@
 import quart
 import datetime
+import os
 from app import config
+try:
+    from app import config
+except ImportError:
+    pass
 
 blueprint = quart.Blueprint("api", __name__)
 
 
 async def calc_exam_timestamp() -> float:
-    tzinfo = datetime.timezone(datetime.timedelta(hours=config.TIMEDELTA))
+    try:
+        exam_month = config.EXAM_MONTH
+        exam_day = config.EXAM_DAY
+        exam_hour = config.EXAM_HOUR
+        exam_min = config.EXAM_MIN
+        timedelta = config.TIMEDELTA
+    except NameError:
+        exam_month = os.environ["EXAM_MONTH"]
+        exam_day = os.environ["EXAM_DAY"]
+        exam_hour = os.environ["EXAM_HOUR"]
+        exam_min = os.environ["EXAM_MIN"]
+        timedelta = os.environ["TIMEDELTA"]
+    tzinfo = datetime.timezone(datetime.timedelta(hours=timedelta))
     curr_datetime = datetime.datetime.now(tz=tzinfo)
     curr_year = curr_datetime.year
     if curr_datetime > datetime.datetime(curr_year,
-                                         config.EXAM_MONTH,
-                                         config.EXAM_DAY,
-                                         config.EXAM_HOUR,
-                                         config.EXAM_MIN,
+                                         exam_month,
+                                         exam_day,
+                                         exam_hour,
+                                         exam_min,
                                          tzinfo=tzinfo):
         exam_datetime = datetime.datetime(curr_year + 1,
-                                          config.EXAM_MONTH,
-                                          config.EXAM_DAY,
-                                          config.EXAM_HOUR,
-                                          config.EXAM_MIN,
+                                          exam_month,
+                                          exam_day,
+                                          exam_hour,
+                                          exam_min,
                                           tzinfo=tzinfo)
     else:
         exam_datetime = datetime.datetime(curr_year,
-                                          config.EXAM_MONTH,
-                                          config.EXAM_DAY,
-                                          config.EXAM_HOUR,
-                                          config.EXAM_MIN,
+                                          exam_month,
+                                          exam_day,
+                                          exam_hour,
+                                          exam_min,
                                           tzinfo=tzinfo)
     return exam_datetime.timestamp()
 
