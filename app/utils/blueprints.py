b22f82848a88fbd7baa30c7b6a2741d1cd4fe937
AquaUseful <l.vladik@outlook.com>
2020-10-26 00:51:53 +0300

    Include api module to app

--- app/utils/blueprints.py
+++ app/utils/blueprints.py
@@ -1,7 +1,7 @@
 from app import app
-from app.modules import index
+from app.modules import index, api
 
 
 async def register_blueprints():
     app.register_blueprint(index.blueprint)
-
+    app.register_blueprint(api.blueprint)
