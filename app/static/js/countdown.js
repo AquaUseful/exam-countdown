87c8990cf882e53484fa89ad22a8419f56597ae2
AquaUseful <l.vladik@outlook.com>
2020-10-26 23:57:53 +0300

    Wrap timer on narrow viewports

--- app/static/js/countdown.js
+++ app/static/js/countdown.js
@@ -8,8 +8,10 @@ function setCounterText(distMilliseconds) {
   let hours = Math.floor(distMilliseconds % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
   let minutes = Math.floor(distMilliseconds % (1000 * 60 * 60) / (1000 * 60));
   let seconds = Math.floor(distMilliseconds % (1000 * 60) / 1000);
-  text = `${days}${DAY_STR} ${hours}${HOUR_STR} ${minutes}${MINUTE_STR} ${seconds}${SECOND_STR}`
-  $(".counter").text(text);
+  $(".counter-days").text(`${days}${DAY_STR}`);
+  $(".counter-hours").text(`${hours}${HOUR_STR}`);
+  $(".counter-minutes").text(`${minutes}${MINUTE_STR}`);
+  $(".counter-seconds").text(`${seconds}${SECOND_STR}`);
 };
 
 function startCountdown(finalDateTime) {
