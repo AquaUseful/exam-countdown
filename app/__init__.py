6f924f373c0cb80a5b9d2549721a202f2872f4e3
AquaUseful <l.vladik@outlook.com>
2020-10-27 01:03:55 +0300

    Fix async __init__ issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 import quart
 import hypercorn
 import os
-import asyncio
-from .utils import blueprints
+from app.utils import blueprints
 
 app = quart.Quart(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
-asyncio.ensure_future(blueprints.register_blueprints(app))
+blueprints.register_blueprints(app)
